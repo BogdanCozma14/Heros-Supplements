b987fdcc0620f9d928c4e768b0e9fe13aec6d948
BogdanCozma14 <116218327+BogdanCozma14@users.noreply.github.com>
2023-09-15 01:17:13 +0300

    Add files via upload

---
+++ static/myscript.js
@@ -0,0 +1,29 @@
+// defining the function for submiting the form when a category was selected
+function submitForm() {
+    document.getElementById("categoryProductsForm").submit();       
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // code for displaying the enlarged images when the user clicks the search button from the menu-images
+    document.querySelectorAll('.menu-portofolio .zoom-button').forEach(image => {
+        image.onclick = (event) => {
+            event.stopPropagation(); // prevent the click event from propagating
+            const parentContainer = image.closest('.image-product');
+            const imageElement = parentContainer.querySelector('.menu-images');
+            const clickedImageSrc = imageElement.getAttribute('src');
+
+            document.querySelector('.enlargeImage').style.display = 'block';
+            document.querySelector('.enlargeImage img').src = clickedImageSrc;
+        }
+    });
+    document.querySelector('.close-enlargedImg').onclick = () => {
+        document.querySelector('.enlargeImage').style.display = 'none';
+    }
+    // event listener for when the user clicks outside the enlarged image
+    document.addEventListener('click', function (event) {
+        if (!event.target.closest('.enlargeImage')) {
+            document.querySelector('.enlargeImage').style.display = 'none';
+        }
+    });   
+});
